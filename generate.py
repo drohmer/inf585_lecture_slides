22ea076ca83dc8cff23239e0f3af745b1571b3f5
Damien Rohmer <damien.rohmer@gmail.com>
2022-12-23 07:16:32 +0100

    Video option

--- generate.py
+++ generate.py
@@ -3,8 +3,8 @@ import yaml
 import argparse
 
 #'plugins/cache_video.py'
-plugins_html = ['plugins/auto-wrap.py', 'plugins/cache_video.py', 'plugins/menu.py', 'plugins/redirection_first_page.py']
-plugins_pdf = ['plugins/auto-wrap.py', 'plugins/cache_video.py', 'plugins/menu.py', 'plugins/generate_pdf.py']
+plugins_html = ['plugins/auto-wrap.py', 'plugins/menu.py', 'plugins/redirection_first_page.py']
+plugins_pdf = ['plugins/auto-wrap.py', 'plugins/menu.py', 'plugins/generate_pdf.py']
 cache_video_directory = '../cache_video/'
 
 source_root_directory = 'src/'
@@ -20,6 +20,7 @@ if __name__== '__main__':
     parser = argparse.ArgumentParser(description='Generate Course Website')
     parser.add_argument('-p','--pdf', action='store_true',help='Add PDF generation.')
     parser.add_argument('-a','--all', action='store_true',help='Force to generate all the source.')
+    parser.add_argument('-v','--videos', action='store_true',help='Add video cache generation.')
     args = parser.parse_args()
 
     if args.all != None:
@@ -30,6 +31,10 @@ if __name__== '__main__':
         compile_dirs = []
         for d in all_dirs:
             compile_dirs.append(d+'/')
+    
+    if args.videos:
+        plugins_html.append('plugins/cache_video.py')
+        plugins_pdf.append('plugins/cache_video.py')
 
 
     for entry_dir in compile_dirs:
