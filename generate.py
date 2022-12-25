f6669e17519e73049e98f981850d9c9618ede043
Damien Rohmer <damien.rohmer@gmail.com>
2022-12-25 16:30:54 +0100

    Update

--- generate.py
+++ generate.py
@@ -2,10 +2,10 @@ import os
 import yaml
 import argparse
 
-#'plugins/cache_video.py'
-plugins_html = ['plugins/auto-wrap.py', 'plugins/menu.py', 'plugins/redirection_first_page.py']
-plugins_pdf = ['plugins/auto-wrap.py', 'plugins/menu.py', 'plugins/generate_pdf.py']
-cache_video_directory = '../cache_video/'
+
+plugins_html = ['static_website_lhtml/plugins/auto-wrap.py', 'static_website_lhtml/plugins/menu.py', 'static_website_lhtml/plugins/redirection_first_page.py']
+plugins_pdf = ['static_website_lhtml/plugins/auto-wrap.py', 'static_website_lhtml/plugins/menu.py', 'static_website_lhtml/plugins/generate_pdf.py']
+cache_video_directory = 'cache_video/'
 
 source_root_directory = 'src/'
 site_directory = '_site/'
@@ -21,7 +21,11 @@ if __name__== '__main__':
     parser.add_argument('-p','--pdf', action='store_true',help='Add PDF generation.')
     parser.add_argument('-a','--all', action='store_true',help='Force to generate all the source.')
     parser.add_argument('-v','--videos', action='store_true',help='Add video cache generation.')
+    parser.add_argument('-d','--debug', action='store_true',help='Debug info.')
     args = parser.parse_args()
+
+    # move to directory
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
     if args.all==True:
         compile_all = True
@@ -33,29 +37,31 @@ if __name__== '__main__':
             compile_dirs.append(d+'/')
     
     if args.videos:
-        plugins_html.append('plugins/cache_video.py')
-        plugins_pdf.append('plugins/cache_video.py')
+        plugins_html.append('static_website_lhtml/plugins/cache_video.py')
+        plugins_pdf.append('static_website_lhtml/plugins/cache_video.py')
 
 
     for entry_dir in compile_dirs:
         source_directory = source_root_directory+entry_dir
 
-        config_html = {'source_directory':'../'+source_directory, 'site_directory':'../'+site_directory+entry_dir+'html/', 'theme':'theme_templates/slides/', 'cache_video_directory': '../cache_video/'+entry_dir, 'plugin':plugins_html}
+        config_html = {'source_directory':source_directory, 'site_directory':site_directory+entry_dir+'html/', 'theme':'static_website_lhtml/theme_templates/slides/', 'cache_video_directory': 'cache_video/'+entry_dir, 'plugin':plugins_html, 'debug':args.debug}
         
-        config_pdf = {'source_directory':'../'+source_directory, 'site_directory':'../'+site_directory+entry_dir+'pdf/', 'theme':'theme_templates/slides-pdf/', 'cache_video_directory': '../cache_video/'+entry_dir, 'plugin':plugins_pdf}
+        config_pdf = {'source_directory':source_directory, 'site_directory':site_directory+entry_dir+'pdf/', 'theme':'static_website_lhtml/theme_templates/slides-pdf/', 'cache_video_directory': 'cache_video/'+entry_dir, 'plugin':plugins_pdf, 'debug':args.debug}
+
+
 
         # HTML
         config_yaml = yaml.dump(config_html)
         with open('configure_temp.yaml','w') as fid:
             fid.write(config_yaml)
-        os.system('cd static_website_lhtml; python generate.py -i ../configure_temp.yaml')
+        os.system(f'python static_website_lhtml/generate.py -i configure_temp.yaml')
 
         # PDF
         if args.pdf==True:
             config_yaml = yaml.dump(config_pdf)
             with open('configure_temp.yaml','w') as fid:
                 fid.write(config_yaml)
-            os.system('cd static_website_lhtml; python generate.py -i ../configure_temp.yaml')
+            os.system(f'python static_website_lhtml/generate.py -i configure_temp.yaml')
 
         # Clean configure_temp
         os.system('rm configure_temp.yaml')
